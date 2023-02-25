b3164ebdf82e513352d49af57078327054a909f6
Roman Faustov <cfar-saba@mail.ru>
2023-02-25 19:56:41 +0200

    move button up

--- helper.py
+++ helper.py
@@ -9,11 +9,3 @@ def stringToJSON(string):
         else:
             class_object += "\nProperty "+ key + " As %String(MAXLEN = \"\");\n"
     return class_object
-
-def jsonToEnsemble(json_data):
-    with open('object.txt', 'w', encoding='iso-8859-8') as f:
-        for key, value in json_data.items():
-            key = key.title()
-            if "_" in key:
-               key = key.replace("_", "")
-            f.write("\nProperty "+ key + " As %String(MAXLEN = \"\");\n")
