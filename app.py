61ae054c75e15ecf3f6611e0d46edf031d53167b
Roman Faustov <cfar-saba@mail.ru>
2023-02-25 19:32:48 +0200

    Actualy do something now

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template, request
 from helper import stringToJSON
 app = Flask(__name__)
 
+data = ""
+jsonString = ""
 @app.route("/", methods=['GET', 'POST'])
 def index():
+
     if request.method == 'POST':
-        data = request.form['data']
+        jsonString = request.form['jsonString']
+        data = request.form['jsonString']
         data = stringToJSON(data)
-        return render_template('base.html', data=data)
-    return render_template('base.html')
+        return render_template('index.html', jsonString = jsonString, classObject=data)
+    else:
+        return render_template('index.html')
+  
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- helper.py
+++ helper.py
@@ -1,4 +1,19 @@
 import json
 
 def stringToJSON(string):
-    return json.loads(string)
+    json_dict =  json.loads(string)
+    class_object = ""
+    for key, value in json_dict.items():
+        if "_" in key:
+            class_object += "\nProperty "+"\"" + key +"\"" +" As %String(MAXLEN = \"\");\n"
+        else:
+            class_object += "\nProperty "+ key + " As %String(MAXLEN = \"\");\n"
+    return class_object
+
+def jsonToEnsemble(json_data):
+    with open('object.txt', 'w', encoding='iso-8859-8') as f:
+        for key, value in json_data.items():
+            key = key.title()
+            if "_" in key:
+               key = key.replace("_", "")
+            f.write("\nProperty "+ key + " As %String(MAXLEN = \"\");\n")
