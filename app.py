f999570d293970318aba87a550bd45d678c10622
Roman Faustov <romanf@assuta.co.il>
2023-02-27 15:32:57 +0200

    test

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def index():
         return render_template('index.html', jsonString = jsonString, classObjects=data)
     else:
         return render_template('index.html')
+    
   
 
 if __name__ == "__main__":
